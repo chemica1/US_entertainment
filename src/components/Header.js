95979033b06ad84240cac378084261b55196b6ab
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-02-13 20:17:15 +0900

    feat : header컴포넌트에 드랍다운과 db연동

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,48 @@ import React from 'react';
 import './Header.css';
 
 import { Dropdown, Menu } from 'semantic-ui-react'
+import firebase from "firebase";
 
-
-const options = [
-  { key: 1, text: 'Choice 1', value: 1 },
-  { key: 2, text: 'Choice 2', value: 2 },
-  { key: 3, text: 'Choice 3', value: 3 },
-]
-
-
-const DropdownExampleSimple = () => (
-    <Dropdown text='자판기목록' options={options} simple item />
-)
+/*
+function fetchDB(uid, DBtitle){
+  const fetchedDB = firebase.database().ref('users/' + uid).child(DBtitle);
+  return fetchedDB;
+}
+*/
 
 
 class Header extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      options :[]
+    }
+    
+    const uid = firebase.auth().currentUser.uid;
+    const fetchedList = firebase.database().ref('users/' + uid +'/vm-list');
+    fetchedList.on('value', snap=>{
+      const DBobject = snap.val();
+      const list = Object.keys(DBobject)
+      const newlist = list.map(x=>{return ({text : x})});
+      this.setState({
+        options: this.state.options.concat(newlist)
+      });
+    });
+  }
+
+
   render() {
     return (
       <div className='header' >
         <div className='left'></div>
         <div className='headerTitle'>{this.props.pageTitle}</div>
-        <div className='right'><DropdownExampleSimple className='dropdown'/></div>
+        <div className='right'>
+        <Dropdown
+         text='자판기목록'
+         options={this.state.options}
+         simple item
+         className='dropdown'/>
+        </div>
 
       </div>
     );
